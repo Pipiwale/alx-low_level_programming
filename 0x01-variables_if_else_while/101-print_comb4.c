4d519ad105c2fa76e0e82b1eac24a4b2522d0d70
Lawrence Elejoka <111017669+Pipiwale@users.noreply.github.com>
2022-09-09 15:53:06 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 /**
@@ -7,28 +8,29 @@
  */
 int main(void)
 {
-		int c, i, k;
-		for (c = '0'; c <= '9'; c++)
-		{
-			for (i = '0'; i <= '9'; i++)
-			{
-				for (k = '0'; k <= '9'; k++)
-				{
-					if (c < i && i < k)
-					{
-						putchar(c);
-						putchar(i);
-						putchar(k);
+	int c, i, k;
 
-						if (c != '7')
-						{
-							putchar(',');
-							putchar(' ');
-						}
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				if (c < i && i < k)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(k);
+
+					if (c != '7')
+					{
+						putchar(',');
+						putchar(' ');
 					}
 				}
 			}
 		}
-		putchar('\n');
-		return (0);
+	}
+	putchar('\n');
+	return (0);
 }
