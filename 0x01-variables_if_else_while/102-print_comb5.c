c132d6b763cc97801fe56f47825e9e35631a00c1
Lawrence Elejoka <111017669+Pipiwale@users.noreply.github.com>
2022-09-09 16:00:41 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,21 +1,47 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include <unistd.h>
 /**
- *  * main - Entry point
- *   * Description: prints all possible combination of 2-digit numbers
- *    * Return: Always 0 (success)
- *     */
+ * main - Entry point
+ * Description: prints all possible combination of 2-digit numbers
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-		int c, i, k, j;
+	int c, i, k, j;
 
-			for (c = 48; c <= 57; c++)
+	for (c = 48; c <= 57; c++)
+	{
+		for (i = 48; i <= 57; i++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				for (j = 48; j <= 57; j++)
+				{
+				if (((k + j) > (c + i) &&  k >= c) || c < k)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(' ');
+					putchar(k);
+					putchar(j);
+
+					if (c + i + k + j == 227 && c == 57)
 					{
-								for (i = 48; i <= 57; i++)
-											{
-															for (k = 48; k <= 57; k++)
-																			{
-																								for (j = 48; j <= 57; j++)
+					break;
+					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}																								for (j = 48; j <= 57; j++)
 																													{
 																																		if (((k + j) > (c + i) &&  k >= c) || c < k)
 																																							{
