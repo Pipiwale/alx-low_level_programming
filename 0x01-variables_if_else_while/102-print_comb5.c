22f2b21c4cc5c09af35eba4ba421666f318db81d
Lawrence Elejoka <111017669+Pipiwale@users.noreply.github.com>
2022-09-09 16:06:16 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -41,30 +41,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-}																								for (j = 48; j <= 57; j++)
-																													{
-																																		if (((k + j) > (c + i) &&  k >= c) || c < k)
-																																							{
-																																													putchar(c);
-																																																		putchar(i);
-																																																							putchar(' ');
-																																																												putchar(k);
-																																																																	putchar(j);
-
-																																																																						if (c + i + k + j == 227 && c == 57)
-																																																																												{
-																																																																																		break;
-																																																																																							}
-																																																																											else
-																																																																																	{
-																																																																																							putchar(',');
-																																																																																												putchar(' ');
-																																																																																																	}
-																																																																															}
-																																						}
-																											}
-																	}
-									}
-				putchar('\n');
-					return (0);
 }
