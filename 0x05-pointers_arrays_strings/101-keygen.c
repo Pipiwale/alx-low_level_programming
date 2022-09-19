05c2c8dfb0e69fb76e2ab3030a7d1a8b88113cc3
Lawrence Elejoka <111017669+Pipiwale@users.noreply.github.com>
2022-09-19 18:18:41 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - entry point generates a random password
  *
@@ -9,16 +8,16 @@
  */
 int main(void)
 {
-	char c;
-	int x;
-	srand(time(0))
+char c;
+int x;
 
-		while (x <= 2645)
-		{
-			c = rand() % 128;
-			x += c;
-			putchar(c);
-		}
-	       putchar(2772 - x);
-	       return (0);
+srand(time(0));
+while (x <= 2645)
+{
+c = rand() % 128;
+x += c;
+putchar(c);
+}
+putchar(2772 - x);
+return (0);
 }
