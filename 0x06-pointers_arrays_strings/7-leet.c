3a1f280ee9e070733e70e724d528d0b1dadbd262
Lawrence Elejoka <111017669+Pipiwale@users.noreply.github.com>
2022-09-21 23:22:59 +0100

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,11 +1,11 @@
-#include "main"
+#include "main.h"
 
 /**
- * leet - Encodes a string into 1337
- * @s: The string to encode
- *
- * Return: The encoded string
- */
+  * leet - Encodes a string into 1337
+  * @s: The string to encode
+  *
+  * Return: The encoded string
+  */
 char *leet(char *s)
 {
 	int a = 0, b = 0, l = 5;
