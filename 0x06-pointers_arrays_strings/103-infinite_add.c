955e872da4cc5506f6e6b36f94838761dd5693c0
Lawrence Elejoka <111017669+Pipiwale@users.noreply.github.com>
2022-09-23 06:24:26 +0100

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,59 +1,60 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-        char *n = "1234567892434574367823574575678477685785645685876876774586734734563456453743756756784458";
-        char *m = "9034790663470697234682914569346259634958693246597324659762347956349265983465962349569346";
-        char r[100];
-        char r2[10];
-        char r3[11];
-        char *res;
+* infinite_add - C function that adds two numbers stored
+*in strings to a buffer.
+*Assumes that strings are never empty and
+*that numbers will always be positive, or 0.
+*Assumes there are only digits stored in the number strings.
+*If result can be stored in the buffer,
+*returns a pointer to the result.
+*If result cannot be stored in the buffer, returns `0`.
+*@n1:first number to be added
+*@n2:second number to be added
+*@r: store result
+*@size_r: size of buffer
+*Return:returns pointer to result
+*/
 
-        res = infinite_add(n, m, r, 100);
-        if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
-        n = "1234567890";
-        m = "1";
-        res = infinite_add(n, m, r2, 10);
-        if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
-        n = "999999999";
-        m = "1";
-        res = infinite_add(n, m, r2, 10);
-        if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
-        res = infinite_add(n, m, r3, 11);
-        if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
-        return (0);
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+}
+
+/**
+* add_strings - Adds the numbers stored in two strings.
+* @n1: The string containing the first number to be added.
+* @n2: The string containing the second number to be added.
+* @r: The buffer to store the result.
+* @r_index: The current index of the buffer.
+*
+* Return: If r can store the sum - a pointer to the result.
+*         If r cannot store the sum - 0.
+*/
+
+char *add_strings(char *n1, char *n2, char *r, int r_index)
+{
+	int num, tens = 0;
+
+	for (; *n1 && *n2; n1--, n2--, r_index--)
+	{
+		num = (*n1 - '0') + (*n2 - '0');
+		num += tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	for (; *n1; n1--; r_index++)
+	{
+		num = *(n1 - '0') + tens; 
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	for (; *n2; n2--;  r_index--)
+	{
+		num = (*n2 - '0') + tens; 
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10; 
+	}
+	
 }
