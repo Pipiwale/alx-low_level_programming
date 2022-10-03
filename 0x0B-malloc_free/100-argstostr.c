da6b11d57a1bdeb3a06bd53021a0bd78dfc96457
Lawrence Elejoka <111017669+Pipiwale@users.noreply.github.com>
2022-10-03 22:10:47 +0100

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,57 +2,51 @@
 #include <stdlib.h>
 
 /**
- * _strlen_recursion - function that returns the length of a string
- * @s: char pointer for string
- * Return: int for length of string
+ * argstostr - concatenates all the arguments of a program.
+ * @ac: argument count.
+ * @av: argument vector.
+ *
+ * Return: pointer of an array of char
  */
-
-int _strlen_recursion(char *s)
-{
-if (*s)
-return (_strlen_recursion(s + 1) + 1);
-
-return (0);
-}
-
-/**
- * argstostr - function that concatenates all the arguments of your program
- * @ac: int for number of arguments
- * @av: char double pointer for array of arguments
- * Return: Char pointer with concatenated arguments, NULL if fail or if
- * arguments passed are NULL or if number of arguments is equal to 0
- */
-
 char *argstostr(int ac, char **av)
 {
-int i, j, l, len = 0, tl = 0;
+	char *aout;
+	int c, i, j, ia;
 
-char *s = NULL;
+	if (ac == 0)
+		return (NULL);
 
-if (ac == 0 || av == NULL)
-return (NULL);
+	for (c = i = 0; i < ac; i++)
+	{
+		if (av[i] == NULL)
+			return (NULL);
 
-for (i = 0; i < ac; i++)
-len += _strlen_recursion(av[i]) + 1;
+		for (j = 0; av[i][j] != '\0'; j++)
+			c++;
+		c++;
+	}
 
-s = malloc(sizeof(char) * len + 1);
+	aout = malloc((c + 1) * sizeof(char));
 
-if (s == NULL)
-return (NULL);
+	if (aout == NULL)
+	{
+		free(aout);
+		return (NULL);
+	}
 
-for (i = 0; i < ac; i++)
-{
-l = _strlen_recursion(av[i]);
+	for (i = j = ia = 0; ia < c; j++, ia++)
+	{
+		if (av[i][j] == '\0')
+		{
+			aout[ia] = '\n';
+			i++;
+			ia++;
+			j = 0;
+		}
+		if (ia < c - 1)
+			aout[ia] = av[i][j];
+	}
+	aout[ia] = '\0';
 
-for (j = 0; j < l; j++)
-s[j + tl] = av[i][j];
-
-s[l + tl] = '\n';
-
-tl += l + 1;
-}
-
-s[tl] = '\0';
-
-return (s);
+	return (aout);
 }
