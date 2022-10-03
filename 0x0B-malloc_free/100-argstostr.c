5897440fee97e82d1fa2cc442f4dc82cc4ea2574
Lawrence Elejoka <111017669+Pipiwale@users.noreply.github.com>
2022-10-03 14:26:27 +0100

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,29 +11,29 @@
 char *argstostr(int ac, char **av)
 {
 	int i, k, n, size;
-		char *str;
+	char *str;
 
-			if (ac == 0 || av == NULL)
-						return (NULL);
-				for (i = 0; i < ac; i++)
-						{
-									for (k = 0; av[i][k] != '\0'; k++)
-													;
-											size += k + 1;
-												}
-					str = malloc(size + 1);
-						if (str == 0)
-									return (NULL);
-							n = 0;
-								for (i = 0; i < ac; i++)
-										{
-													for (k = 0; av[i][k] != '\0'; k++)
-																{
-																				str[n] = av[i][k];
-																							n++;
-																									}
-															str[n] = '\n';
-																	n++;
-																		}
-									return (str);
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (k = 0; av[i][k] != '\0'; k++)
+			;
+		size += k + 1;
+	}
+	str = malloc(size + 1);
+	if (str == 0)
+		return (NULL);
+	n = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (k = 0; av[i][k] != '\0'; k++)
+		{
+			str[n] = av[i][k];
+			n++;
+		}
+		str[n] = '\n';
+		n++;
+	}
+	return (str);
 }
