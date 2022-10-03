e0794fb7ed7d82fb75221859cd2eb1225bea8c45
Lawrence Elejoka <111017669+Pipiwale@users.noreply.github.com>
2022-10-03 22:03:06 +0100

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,10 +9,10 @@
 
 int _strlen_recursion(char *s)
 {
-	if (*s)
-		return (_strlen_recursion(s + 1) + 1);
+if (*s)
+return (_strlen_recursion(s + 1) + 1);
 
-	return (0);
+return (0);
 }
 
 /**
@@ -25,34 +25,34 @@ int _strlen_recursion(char *s)
 
 char *argstostr(int ac, char **av)
 {
-	int i, j, l, len = 0, tl = 0;
+int i, j, l, len = 0, tl = 0;
 
-	char *s = NULL;
+char *s = NULL;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
+if (ac == 0 || av == NULL)
+return (NULL);
 
-	for (i = 0; i < ac; i++)
-		len += _strlen_recursion(av[i]) + 1;
+for (i = 0; i < ac; i++)
+len += _strlen_recursion(av[i]) + 1;
 
-	s = malloc(sizeof(char) * len + 1);
+s = malloc(sizeof(char) * len + 1);
 
-	if (s == NULL)
-		return (NULL);
+if (s == NULL)
+return (NULL);
 
-	for (i = 0; i < ac; i++)
-	{
-		l = _strlen_recursion(av[i]);
+for (i = 0; i < ac; i++)
+{
+l = _strlen_recursion(av[i]);
 
-		for (j = 0; j < l; j++)
-			s[j + tl] = av[i][j];
+for (j = 0; j < l; j++)
+s[j + tl] = av[i][j];
 
-		s[l + tl] = '\n';
+s[l + tl] = '\n';
 
-		tl += l + 1;
-	}
+tl += l + 1;
+}
 
-	s[tl] = '\0';
+s[tl] = '\0';
 
-	return (s);
+return (s);
 }
