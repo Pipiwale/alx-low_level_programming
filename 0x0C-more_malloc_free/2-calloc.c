a9f5f085b3abb3b54035a7345b54c4fa7bf4847a
Lawrence Elejoka <111017669+Pipiwale@users.noreply.github.com>
2022-10-06 18:09:00 +0100

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,32 +1,32 @@
-#include "holberton.h"
+include "holberton.h"
 #include <stdlib.h>
 
 /**
- *   * _calloc - ...
- *     * @nmemb: number of members
- *       * @size: size
- *         *
- *           * Return: ...
- *             */
+  * _calloc - ...
+  * @nmemb: number of members
+  * @size: size
+  *
+  * Return: ...
+  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-		int i = 0, l = 0;
-			char *p;
+	int i = 0, l = 0;
+	char *p;
 
-				if (nmemb == 0 || size == 0)
-							return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-					l = nmemb * size;
-						p = malloc(l);
+	l = nmemb * size;
+	p = malloc(l);
 
-							if (p == NULL)
-										return (NULL);
+	if (p == NULL)
+		return (NULL);
 
-								while (i < l)
-										{
-													p[i] = 0;
-															i++;
-																}
+	while (i < l)
+	{
+		p[i] = 0;
+		i++;
+	}
 
-									return (p);
+	return (p);
 }
