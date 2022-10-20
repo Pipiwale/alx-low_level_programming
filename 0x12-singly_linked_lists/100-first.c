272e66ca64ebb1c45dc5c4cee671e4a69ccd7cf4
Lawrence Elejoka <111017669+Pipiwale@users.noreply.github.com>
2022-10-20 18:42:11 +0100

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,14 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 void first(void) __attribute__ ((constructor));
 
 /**
- *  * first - prints something before main
- *   *
- *    * Return: void
- *     */
+ * first - prints something before main
+ *
+ * Return: void
+ */
 void first(void)
 {
-		printf("You're beat! and yet, you must allow,\n");
-			printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
