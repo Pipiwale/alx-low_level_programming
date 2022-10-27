08a9c3ae26f76e50bee87bfe010026eea3055529
Lawrence Elejoka <111017669+Pipiwale@users.noreply.github.com>
2022-10-27 13:14:44 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,28 +1,50 @@
-#include <stdio.h>
-#include "main.h"
 
+Miguel22247
+/
+holbertonschool-low_level_programming
+Public
+Code
+Issues
+Pull requests
+Discussions
+Actions
+Projects
+Security
+Insights
+holbertonschool-low_level_programming/0x14-bit_manipulation/0-binary_to_uint.c
+@Miguel22247
+Miguel22247 0-binary_to_uint.c
+ 1 contributor
+32 lines (26 sloc)  391 Bytes
+#include "holberton.h"
 /**
- * binary_to_uint - Function that converts a binary number to an unsigned int.
- * Prototype: unsigned int binary_to_uint(const char *b);
- * @b: is pointing to a string of 0 and 1 chars
- * Return: the converted number, or 0 if
- * -> there is one or more chars in the string b that is not 0 or 1
- * -> b is NULL
+ * binary_to_uint - Entry Point
+ * @b: const char
+ * Return: 0
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int value = 0;
+	unsigned int res = 0;
+	int base = 1, i = 0;
 
 	if (b == NULL)
 		return (0);
-	while (*b != '\0')
+
+	while (b[i + 1])
 	{
-		value = value << 1;
-		if (*b != '1' && *b != '0')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		else if (*b == '1')
-			value = value | 1;
-		b++;
+		i++;
 	}
-	return (value);
+
+	while (i >= 0)
+	{
+		res += ((b[i] - '0') * base);
+		base *= 2;
+		i--;
+	}
+
+
+	return (res);
+
 }
